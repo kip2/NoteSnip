11f3fb8375d22527d249aef826e560f2e614d58d
kip2 <tqa1no4@gmail.com>
2024-08-17 20:25:04 +0900

    Modified the generate_hash function to accept a salt value as an argument

--- text-snippet-sharing-service/src/main.rs
+++ text-snippet-sharing-service/src/main.rs
@@ -2,12 +2,12 @@ use blake3::{Hash, Hasher};
 use std::convert::TryInto;
 
 fn main() {
-    generate_hash();
+    todo!()
 }
 
-fn generate_hash() -> Hash {
+fn generate_hash(salt: &[u8]) -> Hash {
     let mut hasher = Hasher::new();
-    hasher.update(b"hello world");
+    hasher.update(salt);
     let result = hasher.finalize();
 
     result
@@ -15,7 +15,7 @@ fn generate_hash() -> Hash {
 
 #[test]
 fn test_generate_hash() {
-    let hash_v = generate_hash();
+    let hash_v = generate_hash(b"hello world");
 
     let expected_hex = "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24";
     let expected_bytes: [u8; 32] = hex::decode(expected_hex)
