ea95ca14a3d9144288a3fcf3697a426a479a970b
kip2 <tqa1no4@gmail.com>
2024-08-17 21:21:47 +0900

    Modify the return type of generate_hash from Hash to String

--- text-snippet-sharing-service/src/hash_util.rs
+++ text-snippet-sharing-service/src/hash_util.rs
@@ -1,36 +1,32 @@
 use blake3::{Hash, Hasher};
 use uuid::Uuid;
 
-pub fn generate_random_hash() -> Hash {
+pub fn generate_random_hash() -> String {
     let uuid = Uuid::new_v4();
     let salt = uuid.as_bytes();
     let random_hash = generate_hash(salt);
     random_hash
 }
 
-fn generate_hash(salt: &[u8]) -> Hash {
+fn generate_hash(salt: &[u8]) -> String {
     let mut hasher = Hasher::new();
     hasher.update(salt);
     let hash = hasher.finalize();
 
-    hash
+    let hash_stirng = hash.to_hex().to_string();
+    hash_stirng
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::convert::TryInto;
 
     #[test]
-    fn test_generate_hash() {
+    fn test_generate_hash_2() {
         let hash_v = generate_hash(b"hello world");
 
-        let expected_hex = "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24";
-        let expected_bytes: [u8; 32] = hex::decode(expected_hex)
-            .expect("Invalid hex string")
-            .try_into()
-            .expect("Expected a 32-byte array");
-        let expected = Hash::from(expected_bytes);
+        let expected =
+            "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24".to_string();
 
         assert_eq!(hash_v, expected);
     }
