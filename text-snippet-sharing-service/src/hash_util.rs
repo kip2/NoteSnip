6ee56518498b9ec27c832aafe312db01630bb796
kip2 <tqa1no4@gmail.com>
2024-08-17 20:43:30 +0900

    Add generate_random_hash function skelton

--- text-snippet-sharing-service/src/hash_util.rs
+++ text-snippet-sharing-service/src/hash_util.rs
@@ -1,12 +1,32 @@
 use blake3::{Hash, Hasher};
 use std::convert::TryInto;
 
+pub fn generate_random_hash() -> Hash {
+    let salt = b"hello world";
+    let random_hash = generate_hash(salt);
+    random_hash
+}
+
 pub fn generate_hash(salt: &[u8]) -> Hash {
     let mut hasher = Hasher::new();
     hasher.update(salt);
-    let result = hasher.finalize();
+    let hash = hasher.finalize();
 
-    result
+    hash
+}
+
+#[test]
+fn test_generate_random_hash() {
+    let hash_v = generate_random_hash();
+
+    let expected_hex = "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24";
+    let expected_bytes: [u8; 32] = hex::decode(expected_hex)
+        .expect("Invalid hex string")
+        .try_into()
+        .expect("Expected a 32-byte array");
+    let expected = Hash::from(expected_bytes);
+
+    assert_eq!(hash_v, expected);
 }
 
 #[test]
