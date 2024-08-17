af1cbb4e487dc3c6efb41296d2ec44928eccf80b
kip2 <tqa1no4@gmail.com>
2024-08-17 22:27:28 +0900

    Create actix-web skelton

--- text-snippet-sharing-service/src/main.rs
+++ text-snippet-sharing-service/src/main.rs
@@ -1,10 +1,23 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
 mod env;
 mod hash;
 mod url;
 
-use url::generate_url;
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, world!"
+}
 
-fn main() {
-    let result = generate_url();
-    println!("{}", result);
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
