0d538e8d212b1a24b417e860edd142215a3f337a
kip2 <tqa1no4@gmail.com>
2024-08-17 20:31:53 +0900

    Extracted generate_hash function to a separate file

---
+++ text-snippet-sharing-service/src/hash_util.rs
@@ -0,0 +1,24 @@
+use blake3::{Hash, Hasher};
+use std::convert::TryInto;
+
+pub fn generate_hash(salt: &[u8]) -> Hash {
+    let mut hasher = Hasher::new();
+    hasher.update(salt);
+    let result = hasher.finalize();
+
+    result
+}
+
+#[test]
+fn test_generate_hash() {
+    let hash_v = generate_hash(b"hello world");
+
+    let expected_hex = "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24";
+    let expected_bytes: [u8; 32] = hex::decode(expected_hex)
+        .expect("Invalid hex string")
+        .try_into()
+        .expect("Expected a 32-byte array");
+    let expected = Hash::from(expected_bytes);
+
+    assert_eq!(hash_v, expected);
+}
--- text-snippet-sharing-service/src/main.rs
+++ text-snippet-sharing-service/src/main.rs
@@ -1,28 +1,9 @@
-use blake3::{Hash, Hasher};
-use std::convert::TryInto;
+use hash_util::generate_hash;
+
+mod hash_util;
 
 fn main() {
-    todo!()
-}
-
-fn generate_hash(salt: &[u8]) -> Hash {
-    let mut hasher = Hasher::new();
-    hasher.update(salt);
-    let result = hasher.finalize();
-
-    result
-}
-
-#[test]
-fn test_generate_hash() {
-    let hash_v = generate_hash(b"hello world");
-
-    let expected_hex = "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24";
-    let expected_bytes: [u8; 32] = hex::decode(expected_hex)
-        .expect("Invalid hex string")
-        .try_into()
-        .expect("Expected a 32-byte array");
-    let expected = Hash::from(expected_bytes);
-
-    assert_eq!(hash_v, expected);
+    let salt = b"hello world";
+    let result = generate_hash(salt);
+    println!("{}", result);
 }
