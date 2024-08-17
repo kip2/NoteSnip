7b3ac6ad2ed915a6eaee899cb4d9b234b2c04ca5
kip2 <tqa1no4@gmail.com>
2024-08-17 21:25:52 +0900

    Create generate_url function

--- text-snippet-sharing-service/src/main.rs
+++ text-snippet-sharing-service/src/main.rs
@@ -3,6 +3,21 @@ use hash_util::generate_random_hash;
 mod hash_util;
 
 fn main() {
-    let result = generate_random_hash();
+    let result = generate_url();
     println!("{}", result);
+}
+
+fn generate_url() -> String {
+    let url_prefix = "https://domain/path/".to_string();
+    let random_hash = generate_random_hash();
+    let url = format!("{}{}", url_prefix, random_hash);
+    url
+}
+
+#[test]
+fn test_generate_domain() {
+    let result = generate_url();
+    let expected = "https://domain/path/";
+
+    assert_eq!(result, expected);
 }
