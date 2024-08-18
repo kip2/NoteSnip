aec27750fa74644e5bdb88c617c5c87ab525d640
kip2 <tqa1no4@gmail.com>
2024-08-18 21:48:24 +0900

    Extracted the JSON-related type definitions and trait

---
+++ text-snippet-sharing-service/src/json.rs
@@ -0,0 +1,41 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct RequestJson {
+    pub snippet: String,
+    pub expiration_stat: String,
+}
+
+#[derive(Serialize)]
+pub struct ResponseJson {
+    pub domain: String,
+}
+
+trait Queryable {
+    fn generate_query(&self) -> String;
+}
+
+impl Queryable for RequestJson {
+    fn generate_query(&self) -> String {
+        "INSERT INTO snippets (domain, snippet, expiration_stat) VALUES (?, ?, ?)".to_string()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_query() {
+        let json = RequestJson {
+            snippet: "test snippet".to_string(),
+            expiration_stat: "etnl".to_string(),
+        };
+
+        let expected =
+            "INSERT INTO snippets (domain, snippet, expiration_stat) VALUES (?, ?, ?)".to_string();
+
+        let result = json.generate_query();
+        assert_eq!(result, expected);
+    }
+}
--- text-snippet-sharing-service/src/main.rs
+++ text-snippet-sharing-service/src/main.rs
@@ -1,47 +1,12 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
-use sqlx::query;
+use json::{RequestJson, ResponseJson};
 use url::generate_url;
 
 mod db;
 mod env;
 mod hash;
+mod json;
 mod url;
-
-#[derive(Deserialize)]
-struct RequestJson {
-    snippet: String,
-    expiration_stat: String,
-}
-
-trait Queryable {
-    fn generate_query(&self) -> String;
-}
-
-impl Queryable for RequestJson {
-    fn generate_query(&self) -> String {
-        "INSERT INTO snippets (domain, snippet, expiration_stat) VALUES (?, ?, ?)".to_string()
-    }
-}
-
-#[test]
-fn test_query() {
-    let json = RequestJson {
-        snippet: "test snippet".to_string(),
-        expiration_stat: "etnl".to_string(),
-    };
-
-    let expected =
-        "INSERT INTO snippets (domain, snippet, expiration_stat) VALUES (?, ?, ?)".to_string();
-
-    let result = json.generate_query();
-    assert_eq!(result, expected);
-}
-
-#[derive(Serialize)]
-struct ResponseJson {
-    domain: String,
-}
 
 #[post("/submit")]
 async fn submit_json(request_data: web::Json<RequestJson>) -> impl Responder {
