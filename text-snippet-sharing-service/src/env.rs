500d2f5fada20234d5624bef74f6cef9a7469662
kip2 <tqa1no4@gmail.com>
2024-08-18 20:15:57 +0900

    Change argument type of read_env_value function

--- text-snippet-sharing-service/src/env.rs
+++ text-snippet-sharing-service/src/env.rs
@@ -1,8 +1,25 @@
 use std::env;
 
 use dotenv::dotenv;
-pub fn read_env_value(key: String) -> String {
+pub fn read_env_value(key: &str) -> String {
     dotenv().expect("Failed to read .env file");
     let env_val = env::var(&key).expect(&format!("{} must be set", &key));
     env_val
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    /// テスト前に以下の準備が必要
+    /// 準備がないままにテストした場合は、テストが失敗することに注意
+    ///
+    /// .envにテスト用のデータの準備が必要
+    /// TEST_VALUE=test_value
+    fn test_read_env_value() {
+        let expected = "test_value";
+        let result = read_env_value("TEST_VALUE");
+        assert_eq!(result, expected);
+    }
 }
--- text-snippet-sharing-service/src/url.rs
+++ text-snippet-sharing-service/src/url.rs
@@ -2,7 +2,7 @@ use crate::env::read_env_value;
 use crate::hash::generate_random_hash;
 
 pub fn generate_url() -> String {
-    let url_prefix = read_env_value("DOMAIN_PREFIX".to_string());
+    let url_prefix = read_env_value("DOMAIN_PREFIX");
     let random_hash = generate_random_hash();
     let url = format!("{}{}", url_prefix, random_hash);
     url
