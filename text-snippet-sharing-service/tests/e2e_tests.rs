91cb42faba1561ca7b2e0597a8c6e25786c30c01
kip2 <tqa1no4@gmail.com>
2024-08-19 21:20:26 +0900

    Add E2E tests

---
+++ text-snippet-sharing-service/tests/e2e_tests.rs
@@ -0,0 +1,50 @@
+use reqwest::Client;
+use serde_json::json;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_api_submit() {
+        let url = "http://127.0.0.1:8080/submit";
+
+        let client = Client::new();
+
+        let request_body = json!({
+            "snippet": "test snippet",
+            "expiration_stat" : "eternal",
+        });
+
+        let response = client
+            .post(url)
+            .json(&request_body)
+            .send()
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(response.status(), 200);
+    }
+
+    #[tokio::test]
+    async fn test_api_submit_error() {
+        let url = "http://127.0.0.1:8080/submit";
+
+        let client = Client::new();
+
+        let request_body = json!({
+            "snippet": "test snippet",
+            // 存在しないステータス
+            "expiration_stat" : "etrnal",
+        });
+
+        let response = client
+            .post(url)
+            .json(&request_body)
+            .send()
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(response.status(), 500);
+    }
+}
