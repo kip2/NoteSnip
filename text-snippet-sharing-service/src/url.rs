8198bf1a1d4811e8079b361ad64b74c969710ae5
kip2 <tqa1no4@gmail.com>
2024-08-19 11:14:40 +0900

    Modified to accepting argument

--- text-snippet-sharing-service/src/url.rs
+++ text-snippet-sharing-service/src/url.rs
@@ -1,10 +1,25 @@
 use crate::env::read_env_value;
-use crate::hash::generate_random_hash;
 use std::error::Error;
 
-pub fn generate_url() -> Result<String, Box<dyn Error>> {
+pub fn generate_url(url_hash: &str) -> Result<String, Box<dyn Error>> {
     let url_prefix = read_env_value("URL_PREFIX")?;
-    let random_hash = generate_random_hash().unwrap();
-    let url = format!("{}{}", url_prefix, random_hash);
+    let url = format!("{}{}", url_prefix, url_hash);
     Ok(url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_hash() {
+        let hash = "123456778900";
+
+        let url_prefix = read_env_value("URL_PREFIX").unwrap();
+        let expected = format!("{}{}", url_prefix, hash);
+
+        let result = generate_url(hash).unwrap();
+
+        assert_eq!(result, expected);
+    }
 }
