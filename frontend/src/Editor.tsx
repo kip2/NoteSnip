7dc1bc440344667bb1d6ec1db30a9a1cf440ff68
kip2 <tqa1no4@gmail.com>
2024-08-29 20:55:44 +0900

    Add a dropdown menu for switching languages

--- frontend/src/Editor.tsx
+++ frontend/src/Editor.tsx
@@ -3,6 +3,7 @@ import { useCallback, useState } from "react";
 import { javascript } from '@codemirror/lang-javascript';
 import { rust } from '@codemirror/lang-rust';
 import { githubDark } from "@uiw/codemirror-themes-all";
+import { Container, NativeSelect } from "@yamada-ui/react";
 
 const Editor = () => {
     const [code, setCode] = useState('console.log("Hello, world")')
@@ -11,15 +12,39 @@ const Editor = () => {
         setCode(val)
     }, [])
 
+    const [language, setLanguage] = useState('rust')
+    const handleLanguageChange = (event) => {
+        setLanguage(event.target.value)
+    }
+
+    const getLanguageExtension = () => {
+        switch(language) {
+            case 'javascript':
+                return javascript({ jsx: true})
+            case 'rust':
+                return rust()
+            default:
+                return rust()
+        }
+    }
+
     return (
-        <CodeMirror
-            value={code}
-            height="700px"
-            // extensions={[javascript({ jsx: true })]}
-            extensions={[rust()]}
-            theme={githubDark}
-            onChange={onCodeChange}
-        />
+        <>
+            <CodeMirror
+                value={code}
+                height="700px"
+                // extensions={[javascript({ jsx: true })]}
+                extensions={[getLanguageExtension()]}
+                theme={githubDark}
+                onChange={onCodeChange}
+            />
+            <Container>
+                <NativeSelect onChange={handleLanguageChange} value={language}>
+                    <option value="javascript">Javascript</option>
+                    <option value="rust">Rust</option>
+                </NativeSelect>
+            </Container>
+        </>
 
     )
 }
