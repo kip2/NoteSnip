786e75db02d78867cf4e8dce3d7f7ed99f2e5871
kip2 <tqa1no4@gmail.com>
2024-08-29 20:39:54 +0900

    Setup Temporary of the Front Page

--- frontend/src/Editor.tsx
+++ frontend/src/Editor.tsx
@@ -1,6 +1,8 @@
 import CodeMirror from "@uiw/react-codemirror";
 import { useCallback, useState } from "react";
 import { javascript } from '@codemirror/lang-javascript';
+import { rust } from '@codemirror/lang-rust';
+import { githubDark } from "@uiw/codemirror-themes-all";
 
 const Editor = () => {
     const [code, setCode] = useState('console.log("Hello, world")')
@@ -12,8 +14,10 @@ const Editor = () => {
     return (
         <CodeMirror
             value={code}
-            height="200px"
-            extensions={[javascript({ jsx: true })]}
+            height="700px"
+            // extensions={[javascript({ jsx: true })]}
+            extensions={[rust()]}
+            theme={githubDark}
             onChange={onCodeChange}
         />
 
--- frontend/src/MainPage.tsx
+++ frontend/src/MainPage.tsx
@@ -1,7 +1,10 @@
-import { Box, Button, MultiSelect, SelectItem, Text, useColorMode, Wrap } from '@yamada-ui/react';
+import { Box, Button, Center, Container, Heading, Text, useColorMode, Wrap } from '@yamada-ui/react';
+import { NativeSelect, NativeOption, NativeOptionGroup } from '@yamada-ui/react';
+import { IconButton } from '@yamada-ui/react';
 import { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import Editor from './Editor';
+import { Sun } from '@yamada-ui/lucide';
 
 const MainPage = () => {
     const [response, setResponse] = useState('');
@@ -30,7 +33,7 @@ const MainPage = () => {
         .catch(error => console.error('Error fetching data:', error))
     }
 
-    const items: SelectItem[] = [
+    const items: NativeSelectItem[] = [
         { label: "1min", value: "1min"},
         { label: "10min", value: "10min"},
         { label: "1day", value: "1day" },
@@ -42,35 +45,32 @@ const MainPage = () => {
 
     return (
         <>
-            <Wrap gap="ld">
-                <Button onClick={() => changeColorMode("light")}>ライトモード</Button>
-                <Button onClick={() => changeColorMode("dark")}>ダークモード</Button>
-                <Button onClick={() => changeColorMode("system")}>システム</Button>
-            <Box >
-                <h1>Main Page</h1>
-                <Text>こんにちは、世界!</Text>
-                <Button
-                    isLoading
-                    loadingPlacement='end'
-                    loadingIcon="grid"
-                    colorScheme='sky'
-                    size="lg"
-                    m={5}
-                >
-                    Click me!
-                </Button>
-                <MultiSelect 
-                focusBorderColor='green.500'
-                maxW="xs"
-                placeholder='有効期限を選択' items={items} />
-            </Box>
-            </Wrap>
-            <Box>
-                <Text>
-                    ほぅ...青ざめた血ねぇ...
-                </Text>
-                <Editor></Editor>
-            </Box>
+            <Container size="ld">
+                <Center>
+                    <Heading>Snippet Sharing Service(SSS(仮))</Heading>
+                </Center>
+                <Box gap="ms">
+                    <Editor></Editor>
+                </Box>
+                
+                <Center>
+                    <Box>
+                        <NativeSelect
+                            focusBorderColor='green.500'
+                            maxW="xs"
+                            placeholder='有効期限を選択' 
+                            items={items} />
+                    </Box>
+                </Center>
+                <Center>
+                    <Box display="flex" gap="md">
+                        <Button onClick={() => changeColorMode("light")}>ライトモード</Button>
+                        <Button onClick={() => changeColorMode("dark")}>ダークモード</Button>
+                        <Button onClick={() => changeColorMode("system")}>システム</Button>
+                        <IconButton icon={<Sun />} />
+                    </Box>
+                </Center>
+            </Container>
         </>
     )
 }
