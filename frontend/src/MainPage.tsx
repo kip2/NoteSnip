7287cd79198ae66befe9f00a54b2991df41a00c6
kip2 <tqa1no4@gmail.com>
2024-09-10 21:14:44 +0900

    Add skelton for DB access processing using hash

--- frontend/src/MainPage.tsx
+++ frontend/src/MainPage.tsx
@@ -1,15 +1,49 @@
-import { Box,  Container, useColorModeValue} from '@yamada-ui/react';
+import { Box,  Button,  Container, useColorModeValue} from '@yamada-ui/react';
 import { useParams } from 'react-router-dom';
 import Editor from './Editor/Editor';
 import { RegisterSubmitButton } from './Button/RegisterSubmit';
 import Header from './Header/Header';
+import { useEffect, useRef } from 'react';
 
 const MainPage = () => {
     // URLパラメータからハッシュ値を取得
     const params = useParams<{ hash?: string}>()
     const hash = params.hash
+
     const bg = useColorModeValue("white", "neutral.900")
 
+    const isFirstRender = useRef(true)
+
+    const fetchDataByHash = async () => {
+        if (!hash) return
+
+        const response = await fetch(`http://127.0.0.1:8000/get/${hash}`, {
+            method: "GET",
+        })
+
+        if (!response.ok) {
+            console.error("Error fetching data:", response.statusText)
+            return
+        }
+
+        const data = await response.json()
+        console.log(data)
+    }
+
+    useEffect(() => {
+        if (isFirstRender.current && hash) {
+            fetchDataByHash()
+            console.log("test")
+        }
+
+        // 初回のみの動作にするため、useRefを更新する
+        isFirstRender.current = false
+    }, [])
+
+
+    const handleClick = () => {
+        console.log("hash:",hash)
+    }
     return (
         <Box bg={bg}>
             <Header></Header>
@@ -18,6 +52,7 @@ const MainPage = () => {
                     <Editor></Editor>
                 </Box>
                 <RegisterSubmitButton/>
+                <Button onClick={handleClick} >テスト</Button>
             </Container>
         </Box>
     )
