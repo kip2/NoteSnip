2d612d762e12ce518e387ecc59af9f3310d1ee8e
kip2 <tqa1no4@gmail.com>
2024-09-16 13:17:28 +0900

    Add build.sh

---
+++ backend/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+THRESHOLD=10
+
+USED_DISK=$(df --output=used / | tail -n 1 | awk '{print $1 / 1024 / 1024}')
+
+if (( $(echo "$USED_DISK > $THRESHOLD" | bc -l) )); then
+    echo "Disk usage is above ${THRESHOLD}GB, running docker system prune -a"
+    yes | docker system prune -a
+fi
+
+docker-compose build --no-cache && docker-compose up
+
+sudo chmod +x /target/x86_64-unknown-linux-gnu/release/notesnip-api-server
